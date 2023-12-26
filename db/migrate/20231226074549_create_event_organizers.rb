6031c0a564b5e5613a04ecec025521d9742550fc
Srijan Kapoor <srijan@knyfe.co>
2023-12-26 13:21:32 +0530

    Add class EventOrganizer

---
+++ db/migrate/20231226074549_create_event_organizers.rb
@@ -0,0 +1,13 @@
+class CreateEventOrganizers < ActiveRecord::Migration[6.0]
+  def change
+    create_table :event_organizers do |t|
+      t.string :email
+      t.string :name
+      t.string :password_digest
+
+      t.timestamps
+    end
+
+    add_index :event_organizers, :email, unique: true
+  end
+end
