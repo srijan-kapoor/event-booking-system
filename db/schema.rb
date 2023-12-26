5d9edcce1f0bd627fb0d258ae2a299cf31bc6ecd
Srijan Kapoor <srijan@knyfe.co>
2023-12-26 13:23:00 +0530

    Add class Customer

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_12_26_074549) do
+ActiveRecord::Schema.define(version: 2023_12_26_074833) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
   enable_extension "plpgsql"
+
+  create_table "customers", force: :cascade do |t|
+    t.string "email"
+    t.string "name"
+    t.string "password_digest"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["email"], name: "index_customers_on_email", unique: true
+  end
 
   create_table "event_organizers", force: :cascade do |t|
     t.string "email"
